0d61a602463809ded54fd3d6121a5dc4722c9185
guillemeet <gmolinallado@gmail.com>
2024-03-05 19:21:59 +0100

    Changed subquery to optimal query

--- src/main/java/com/inditex/priceapi/infraestructure/adapters/output/repository/PriceRepository.java
+++ src/main/java/com/inditex/priceapi/infraestructure/adapters/output/repository/PriceRepository.java
@@ -13,10 +13,8 @@ public interface PriceRepository extends JpaRepository<PriceEntity, Long> {
             "WHERE p.brandId = :brand_id " +
             "AND p.productId = :product_id " +
             "AND :application_date BETWEEN p.startDate AND p.endDate " +
-            "AND p.priority = (SELECT MAX(p2.priority) FROM PriceEntity p2 " +
-            "WHERE p2.brandId = :brand_id " +
-            "AND p2.productId = :product_id " +
-            "AND :application_date BETWEEN p2.startDate AND p2.endDate)")
+            "ORDER BY p.priority DESC " +
+            "LIMIT 1 ")
     PriceEntity findPrice(
             @Param("brand_id") Integer brandId,
             @Param("product_id") Integer productId,
